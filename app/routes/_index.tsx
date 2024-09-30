685ff1d9a5153e432d6f9e91bae8c1c83b7a0a78
Filip <szymanski.fp@gmail.com>
2024-09-30 19:16:54 +0200

    dummy auth

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,12 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  json,
+  redirect,
+  type ActionFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import { LoaderCircle } from "lucide-react";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -11,12 +18,41 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const form = await request.formData();
+
+  const email = String(form.get("email"));
+  const password = String(form.get("password"));
+
+  await new Promise((res) => setTimeout(res, 1000));
+
+  if (email === "a@a.com") {
+    return json({
+      errors: {
+        email: "Invalid email",
+      },
+    });
+  }
+
+  if (email.length > 0 && password.length > 0) {
+    return redirect("/home");
+  }
+
+  return null;
+}
+
 export default function Index() {
-  const isLoading = false;
+  const actionData = useActionData<typeof action>();
+  const navigation = useNavigation();
+
+  const [password, setPassword] = useState<string>(
+    actionData?.errors.email !== undefined ? "" : ""
+  );
 
   return (
-    <div className="">
-      <form>
+    <div className="min-h-screen flex flex-row items-center justify-center">
+      <Form method="post">
+        <h1>Sign in</h1>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only">Email</Label>
@@ -24,31 +60,32 @@ export default function Index() {
               id="email"
               placeholder="Email"
               type="email"
-              disabled={isLoading}
+              name="email"
+              disabled={navigation.state === "submitting"}
             />
             <Input
               id="email"
+              name="password"
               placeholder="Password"
               type="password"
-              disabled={isLoading}
+              disabled={navigation.state === "submitting"}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
-          <Button type="submit" disabled={isLoading}>
-            {isLoading && <LoaderCircle />}
-            Sign In with Email
+          {actionData?.errors && (
+            <p className="text-sm text-red-500">Incorrect Email or Password</p>
+          )}
+          <Button type="submit" disabled={navigation.state === "submitting"}>
+            <span className="flex flex-row items-center justify-center leading-3">
+              {navigation.state === "submitting" && (
+                <LoaderCircle className="animate-spin mb-0.5" size={18} />
+              )}
+              <span className="leading-3">Sign In</span>
+            </span>
           </Button>
         </div>
-        <div className="relative mt-2">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background text-muted-foreground px-2">
-              Or continue with
-            </span>
-          </div>
-        </div>
-      </form>
+      </Form>
     </div>
   );
 }
