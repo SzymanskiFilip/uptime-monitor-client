2b7317a111f10a7ba10fd9c505340a8fab99352d
Filip <szymanski.fp@gmail.com>
2024-10-02 17:38:20 +0200

    initial crud

--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -1,16 +1,45 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useActionData, useLoaderData } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
 import { validate } from "~/lib/auth.server";
 import { DomainsList } from "./domains_list";
+import { Domain } from "~/types/api-types";
+import { deleteDomain, saveDomain } from "~/service/domains.server";
+import { useEffect } from "react";
+import { useToast } from "~/hooks/use-toast";
+import { Toaster } from "~/components/ui/toaster";
 
 export async function loader(request: LoaderFunctionArgs) {
   await validate(request);
 
-  return null;
+  const res = await fetch("http://localhost:1323/domains", {
+    method: "GET",
+  });
+
+  const body: Domain[] = await res.json();
+
+  return json({
+    domains: body,
+  });
 }
 
 export default function Home() {
+  const data = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (actionData !== undefined && actionData !== null) {
+      if (actionData.saveDomainStatus === "invalid") {
+        toast({
+          title: "Input a valid url.",
+          variant: "destructive",
+        });
+      }
+    }
+  }, [actionData]);
+
   return (
     <>
       <nav className="flex flex-row items-center justify-between p-4">
@@ -22,10 +51,31 @@ export default function Home() {
       <div className="flex flex-row items-center justify-center">
         <div className="max-w-[1400px] w-full">
           <section className="flex flex-row items-center justify-start">
-            <DomainsList />
+            <DomainsList domains={data.domains} />
           </section>
         </div>
       </div>
+      <Toaster />
     </>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+
+  const obj = Object.fromEntries(formData);
+
+  if (obj.action === "domain-save") {
+    const saveDomainStatus = await saveDomain(formData);
+    console.log(formData);
+    return json({
+      saveDomainStatus: saveDomainStatus,
+    });
+  }
+
+  if (obj.action === "domain-delete") {
+    await deleteDomain(formData);
+  }
+
+  return null;
 }
---
+++ app/service/domains.server.ts
@@ -0,0 +1,42 @@
+export async function saveDomain(
+  formData: FormData
+): Promise<"success" | "invalid" | "fail"> {
+  await new Promise((res) => setTimeout(res, 1000));
+
+  if (!isValidURL(formData.get("domain"))) {
+    return "invalid";
+  }
+
+  const res = await fetch("http://localhost:1323/domains", {
+    method: "POST",
+    body: JSON.stringify({ url: String(formData.get("domain")) }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (res.status === 400) {
+    return "fail";
+  }
+
+  if (res.status === 200) {
+    return "success";
+  }
+
+  //already exists
+  if (res.status === 409) {
+    return "success";
+  }
+
+  return "fail";
+}
+
+export async function deleteDomain(formData: FormData) {
+  console.log(formData);
+}
+
+function isValidURL(url) {
+  const regex =
+    /^(https?:\/\/)(localhost|(([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,})(:\d{1,5})?(\/.*)?$/;
+  return regex.test(url);
+}
