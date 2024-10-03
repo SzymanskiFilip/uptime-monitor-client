ab98132f34e013dc7fb2ddb991368aa446613c7f
Filip <szymanski.fp@gmail.com>
2024-10-03 23:38:56 +0200

    dummy list

--- app/routes/server.$id/general_data_list.tsx
+++ app/routes/server.$id/general_data_list.tsx
@@ -1,3 +1,33 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+
 export function GeneralDataList() {
-  return <>list</>;
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[180px]">Sent at</TableHead>
+          <TableHead className="w-[80px]">Status</TableHead>
+          <TableHead>Response Headers</TableHead>
+          <TableHead className="text-right">Response Time</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
 }
