e896984bc9c4d8ad04b083cbb04d023967f313e1
Filip <szymanski.fp@gmail.com>
2024-10-03 18:31:31 +0200

    next ones

---
+++ app/routes/server.$id/response_time.tsx
@@ -0,0 +1,3 @@
+export function ResponseTimeWeek() {}
+
+export function ResponseTimeMonth() {}
--- app/routes/server.$id/route.tsx
+++ app/routes/server.$id/route.tsx
@@ -2,6 +2,7 @@ import { LoaderFunctionArgs } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { House } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { StatsGathered } from "../home/stats_gathered";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   console.log(params);
@@ -23,6 +24,27 @@ export default function ServerID() {
           Home
         </Link>
       </Button>
+
+      <div className="flex flex-row items-center justify-center">
+        <div className="max-w-[1400px] w-full">
+          <section>
+            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+              Domain:{" "}
+              <span className="text-red-400">
+                https://localhost:3000/api/test
+              </span>
+              {" Id: "}
+              <span className="text-blue-400">
+                8b91d2b2-daef-4e75-94ef-413e7111562e
+              </span>
+            </h2>
+          </section>
+
+          <section></section>
+
+          <section></section>
+        </div>
+      </div>
     </>
   );
 }
