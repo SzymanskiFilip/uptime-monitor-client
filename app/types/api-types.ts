58e81e601c880476a18dba560615938fc5a1e9b2
Filip <szymanski.fp@gmail.com>
2024-10-03 23:40:22 +0200

    api type

--- app/types/api-types.ts
+++ app/types/api-types.ts
@@ -2,3 +2,32 @@ export type Domain = {
   id: string;
   url: string;
 };
+
+export type URLDetails = {
+  url: string;
+  id: string;
+  response_times: {
+    date: Date;
+    avg: number;
+  }[];
+  minimum: Statistic;
+  maximum: Statistic;
+  response_times_7: {
+    date: Date;
+    avg: number;
+  }[];
+  response_times_14: {
+    date: Date;
+    avg: number;
+  }[];
+  outages: Statistic[];
+};
+
+type Statistic = {
+  id: string;
+  url_id: string;
+  headers: string;
+  success: boolean;
+  response_time: number;
+  saved_at: Date;
+};
